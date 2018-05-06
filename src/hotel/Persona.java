3f5fc69928a53a4dc0b080573a6e051fcfd88654
OLMEDO <OLMEDO@192.168.100.4>
2018-05-06 18:04:40 -0500

    Anniadidas clase Cliente, Usuario, Persona y listaUsuarios

---
+++ src/hotel/Persona.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author OLMEDO <00097017@uca.edu.sv>
+ */
+
+public class Persona {
+    public String nombre;
+    public String dui;
+    public String telefono;
+    public String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.dui, other.dui)) {
+            return false;
+        }
+        return true;
+    }  
+}
