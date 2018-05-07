bbffd5088d1fb1d497b811099f4da2b7f4577de0
Jeniffer <Jeniffer@LAPTOP-6MT5MPRP>
2018-05-06 18:32:03 -0600

    lista de servicios que incluye el hotelcon la funcion de agregar modificar.

---
+++ src/hotel/ListaServicios.java
@@ -0,0 +1,98 @@
+
+package hotel;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jeniffer Merino
+ */
+public class ListaServicios {
+
+    Scanner entrada = new Scanner(System.in);
+
+    ArrayList<Servicio> listaServicio = new ArrayList<>();
+
+    public ListaServicios(String nombrePaquete) {
+        if (nombrePaquete == "no") {
+
+        }
+        if (nombrePaquete == "Premium") {
+            Servicio servicio1 = new Servicio("Buffet Desayuno");
+            Servicio servicio2 = new Servicio("Acceso ilimitado a piscina");
+            Servicio servicio3 = new Servicio("Servicio a la habitacion");
+            Servicio servicio4 = new Servicio("Acceso ilimitado al minibar");
+            Servicio servicio5 = new Servicio("internet ilimitado");
+
+            listaServicio.add(servicio1);
+            listaServicio.add(servicio2);
+            listaServicio.add(servicio3);
+            listaServicio.add(servicio4);
+            listaServicio.add(servicio5);
+        }
+        if (nombrePaquete == "Basico") {
+
+            Servicio servicio2 = new Servicio("Acceso ilimitado a piscina");
+            Servicio servicio5 = new Servicio("internet ilimitado");
+
+            listaServicio.add(servicio2);
+            listaServicio.add(servicio5);
+
+        }
+        if (nombrePaquete == "custom") {
+            Scanner entrada2 = new Scanner(System.in);
+            System.out.println("Cuantos servicios quiere?");
+            int n = entrada.nextInt();
+            for (int i = 1; i <=n; i++) {
+                System.out.println("Ingrese el nombre del servicio");
+                String nuevoSer=entrada2.nextLine();
+                Servicio servicio = new Servicio(nuevoSer);
+                listaServicio.add(servicio);
+                System.out.println("Se agrego con exito el servicio");
+            }
+
+        }
+
+    }
+
+
+public void AgregarNuevoServicio() {
+        System.out.println("Ingrese el nombre del nuevo servicio");
+        Servicio servicio = new Servicio(entrada.nextLine());
+        for (Servicio ser : listaServicio) {
+            if (ser.equals(servicio)) {
+                System.out.println("Este servicio ya existe");
+                return;
+            }
+        }
+        listaServicio.add(servicio);
+        System.out.println("Se agrego con exito el servicio");
+    }
+
+    public String mostrar() {
+        String palabras = "";
+        for (Servicio s : listaServicio) {
+            palabras += ", " + s.getNombre();
+        }
+        return palabras;
+    }
+
+    public void eliminar() {
+        System.out.println("que servicio desea eliminar");
+        Servicio servicio = new Servicio(entrada.nextLine());
+        int pos = 0;
+
+        for (Servicio ser : listaServicio) {
+            if (ser.equals(servicio)) {
+                listaServicio.remove(pos);
+                System.out.println("Servicio eliminado");
+                return;
+            }
+            pos = pos + 1;
+
+        }
+        System.out.println("Servicio no encontrado");
+    }
+}
+
